f60efd493fce036b9dc40ee3abeecfe4b793c57d
GuilhermeSM <guilhermesm.0808@gmail.com>
2025-09-04 22:42:39 -0300

    Aula 05 - Routes usando React

--- Aula 5 - Imagens e Rotas/react-tailwind/src/pages/Dashboard.jsx
+++ Aula 5 - Imagens e Rotas/react-tailwind/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import Tabs from "../components/Tabs";
+import logo from "../assets/logo.png"
 
 export default function Dashboard() {
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard</h1>
+      <img src={logo} alt="" />
       <Tabs />
     </div>
   );
